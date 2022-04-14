ece71a802b145803156136627f42f0585635817a
Rasmus Kaj <kaj@kth.se>
2022-04-14 16:49:39 +0200

    Improve relative path handling for `@use`
    This should fix some problems with relative paths in `@use`, while
    making some more error messages dart-sass compatible and maybe
    exposing a windows path problem.

--- tests/spec/directives/test_use/error/load.rs
+++ tests/spec/directives/test_use/error/load.rs
@@ -313,7 +313,6 @@ mod test_loop {
     }
 }
 #[test]
-#[ignore] // wrong error
 fn missing() {
     let runner = runner().with_cwd("missing");
     assert_eq!(
@@ -341,7 +340,6 @@ fn no_extension() {
     );
 }
 #[test]
-#[ignore] // wrong error
 fn unknown_scheme() {
     let runner = runner().with_cwd("unknown_scheme");
     assert_eq!(
